12066103b39a906719a5059e015d135f52d46e6b
Semjon Zyubin <lesqt58@gmail.com>
2024-02-09 20:35:19 +0400

    lesson 15

--- app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -1,9 +1,13 @@
 package com.example.shoppinglist.domain
 
 data class ShopItem(
-    var name: String,
-    val id: Int,
-    var count: Int,
-    var isActive: Boolean
+    val name: String,
+    val count: Int,
+    val isActive: Boolean,
+    var id: Int = UNDEFINED_ID,
 ) {
+
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
 }
