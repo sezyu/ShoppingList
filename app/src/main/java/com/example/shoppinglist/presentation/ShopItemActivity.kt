2ba95087194f04389849777cf2cdd183a81df8a8
Semjon Zyubin <lesqt58@gmail.com>
2024-02-12 20:56:59 +0400

    lesson 44

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.example.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.shoppinglist.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.shoppinglist.data.ShopListRepositoryImpl
 import com.example.shoppinglist.domain.AddShopItemUseCase
 import com.example.shoppinglist.domain.EditShopItemUseCase
 import com.example.shoppinglist.domain.GetShopItemUseCase
-import com.example.shoppinglist.domain.GetShopListUseCase
 import com.example.shoppinglist.domain.ShopItem
 
 class ShopItemViewModel: ViewModel() {
@@ -16,9 +17,25 @@ class ShopItemViewModel: ViewModel() {
     private val editShopItemUseCase = EditShopItemUseCase(repository)
     private val addShopItemUseCase = AddShopItemUseCase(repository)
 
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName:LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount:LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem:LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
 
     fun getShopItem(shopItemId: Int){
         val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
     }
 
     fun addShopItem(inputName: String?, inputCount: String?){
@@ -28,6 +45,7 @@ class ShopItemViewModel: ViewModel() {
         if (fieldsValid){
             val shopItem = ShopItem(name,count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
         }
 
     }
@@ -37,8 +55,11 @@ class ShopItemViewModel: ViewModel() {
         val count = parseCount(inputCount)
         val fieldsValid = validateInput(name,count)
         if (fieldsValid){
-            val shopItem = ShopItem(name,count, true)
-            editShopItemUseCase.editShopItem(shopItem)
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork() }
+
         }
 
     }
@@ -57,15 +78,26 @@ class ShopItemViewModel: ViewModel() {
     private fun validateInput(name: String, count: Int):Boolean{
         var result = true
         if (name.isBlank()){
-            //TODO: show error input name
+            _errorInputName.value = true
             result = false
         }
 
         if (count <= 0){
-            //TODO: show error input count
+            _errorInputCount.value = true
             result = false
         }
         return result
+    }
+
+    fun resetErrorInputTypeName(){
+        _errorInputName.value = false
+    }
+    fun resetErrorInputTypeCount(){
+        _errorInputCount.value = true
+    }
+
+    private fun finishWork(){
+        _shouldCloseScreen.value = Unit
     }
 
 }
